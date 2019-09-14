abea3c3d5d9a5b8fdaf4da760ea7d1807594b351
Alain <davisbedetse@gmail.com>
2019-09-14 17:26:10 -0400

    aws fix

--- services/fileUpload.js
+++ services/fileUpload.js
@@ -13,7 +13,7 @@ const s3 = new aws.S3();
 const upload = multer({
 	storage: multerS3({
 		s3,
-		bucket: 'staff-manager',
+		bucket: 'staff-app-bucket',
 		acl: 'public-read',
 		metadata: function(req, file, cb) {
 			cb(null, { fieldName: file.fieldname });
